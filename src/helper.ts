54c951e1f916c257ed82bafa8806861c75cd07a2
MakrandMZare <makzare1463@gmail.com>
2023-06-15 11:49:18 +0530

    Initial Commit

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,6 @@
 export const CheckWebGPU = () => {
     let result = 'Great, your current browser supports WebGPU';
-        if (!navigator) {
+        if (!navigator.gpu) {
             result = 'Your current browser does not suposrt WebGPU! Make sure you are on a system with WebGPU enabled. Currently, SPIR-WebGPU is only supported in <a href="https://www.google.com/chrome/canary">Chrome canary</a> with the flag "enable-unsafe-webgpu" enabled. See the <a href="https://github.com/gpuweb/wiki/Implementation-Status"> Implementation Status</a> page for more details.';
         }
     return result;
